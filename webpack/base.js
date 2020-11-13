6c601069e374588cc5cf7c3b6389a650237247bb
tGodson <tendongze95@gmail.com>
2020-11-13 01:22:35 +0100

    deploy game to netlify

--- webpack/base.js
+++ webpack/base.js
@@ -39,14 +39,14 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin({
-      root: path.resolve(__dirname, '../'),
+      root: path.resolve(__dirname, './'),
     }),
     new webpack.DefinePlugin({
       CANVAS_RENDERER: JSON.stringify(true),
       WEBGL_RENDERER: JSON.stringify(true),
     }),
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: './index.html'
     }),
   ],
 };
--- webpack/prod.js
+++ webpack/prod.js
@@ -5,7 +5,7 @@ const base = require('./base');
 module.exports = merge(base, {
   mode: 'production',
   output: {
-    filename: 'bundle.min.js',
+    filename: 'bundle.min.js'
   },
   devtool: false,
   performance: {
