69f5462c88234c5f6e9c507c6460241b4d487f53
tGodson <tendongze95@gmail.com>
2020-11-13 10:38:51 +0100

    fix all es6 errors

--- webpack/base.js
+++ webpack/base.js
@@ -51,7 +51,7 @@ module.exports = {
       WEBGL_RENDERER: JSON.stringify(true),
     }),
     new HtmlWebpackPlugin({
-      template: './index.html'
+      template: './index.html',
     }),
   ],
 };
